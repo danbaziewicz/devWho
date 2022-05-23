5904b58ab09e420b4e2a4d115530b8c5cc5e4c2b
danbaziewicz <danxbazi@gmail.com>
2022-05-23 00:09:25 -0300

    feat: criada validação e comparação de senhas.

--- js/models/validacoes.js
+++ js/models/validacoes.js
@@ -6,16 +6,50 @@ class Validacao {
     }
 
     validaEmail() {
-        const emailReq= /^.+@.+\..{2,}$/;
-		const proibidoUso = /[\(\)\<\>\,\;\:\\\/\"\[\]]/;
+        const emailReq = /^.+@.+\..{2,}$/;
+        const proibidoUso = /[\(\)\<\>\,\;\:\\\/\"\[\]]/;
         this.getEmail();
-        if(!(emailReq.test(this.email)) || this.email.match(proibidoUso)){
+        if (!(emailReq.test(this.email)) || this.email.match(proibidoUso)) {
             const reqImprime = new ControllerCadastro();
             reqImprime.imprimeStatusInvalido();
 
         } else {
             const reqImprime = new ControllerCadastro();
             reqImprime.imprimeStatusOk();
+        }
+    }
+
+    validaSenha() {
+        this.getSenha();
+        if (this.senha == "") {
+            const reqErro = new ControllerCadastro();
+            reqErro.imprimeSenhaErrada();
+        } else {
+            const numeral = /([0-9])/;
+            const letras = /([a-zA-Z])/;
+            const simbolos = /([~,!,@,#,$,%,^,&,*,-,_,+,=,?,>,<])/;
+            if (this.senha.length < 8) {
+                const reqErro = new ControllerCadastro();
+                reqErro.imprimeSenhaErrada();
+            } else if (this.senha.match(numeral) && this.senha.match(letras) && this.senha.match(simbolos)) {
+                const reqSenhaOk = new ControllerCadastro();
+                reqSenhaOk.imprimeSenhaOk();
+            } else {
+                const reqErro = new ControllerCadastro();
+                reqErro.imprimeSenhaErrada();
+            }
+        }
+    }
+
+    comparaSenhas() {
+        this.getSenha();
+        this.getConfirmacaoSenha();
+        if (this.senha == this.confirmaSenha) {
+            const reqOk = new ControllerCadastro();
+            reqOk.imprimeConfirmOk();
+        } else {
+            const reqDiv = new ControllerCadastro();
+            reqDiv.imrpimeConfirmDiv();
         }
     }
 
@@ -28,8 +62,6 @@ class Validacao {
     }
 
     getConfirmacaoSenha() {
-        this.confirmaSenha = $('##inputConfirmPassword4')
+        this.confirmaSenha = $('#inputConfirmPassword4').val();
     }
-
-    //#inputPassword4 #inputConfirmPassword4
 }
