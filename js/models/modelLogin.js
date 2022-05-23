ef3a6ced6931bc2a87ed0b4a7a4d8d5f18ca8944
danbaziewicz <danxbazi@gmail.com>
2022-05-23 07:47:56 -0300

    feat: criada verificação de cammpos email e senha

--- js/models/modelLogin.js
+++ js/models/modelLogin.js
@@ -1,14 +1,41 @@
-class ModelLogin {
+class UserLogin {
     constructor() {
         this.email = '';
         this.senha = '';
     }
 
+    verificaCampos() {
+        const checkEmail = this.verificaEmail();
+        const checkSenha = this.verificaSenha();
+        if (checkEmail == false) {
+            const invalido = new UserLoginController();
+            invalido.reqLoginEmailInvalido();
+        } else if(checkSenha == false) {
+            const senhaInvalida = new UserLoginController();
+            senhaInvalida.reqLoginSenhaInvalida();
+        } else if ((checkEmail) && (checkSenha)) {
+            const valido = new UserLoginController();
+            valido.reqLoginOk();
+        }
+    }
+
+
+    verificaSenha() {
+        this.getSenha();
+        return this.senha.length >= 8 && this.senha !== '' ? true : false;
+    }
+
+    verificaEmail() {
+        this.getEmail();
+        const emailReq = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        return this.email.match(emailReq) ? true : false;
+    }
+
     getEmail() {
-        this.email = $('#InputEmailLogin').val();
+        this.email = $('#inputEmailLogin').val();
     }
 
     getSenha() {
-        this.senha = $('#InputPassword').val();
+        this.senha = $('#inputPassword').val();
     }
 }
