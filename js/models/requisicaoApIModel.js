3d6c5e1e97f7b8696e0aab446478bb9061e211cc
danbaziewicz <danxbazi@gmail.com>
2022-05-22 21:21:24 -0300

    fix: ajustado requisição de impressão de erro e condição request.

--- js/models/requisicaoApIModel.js
+++ js/models/requisicaoApIModel.js
@@ -11,28 +11,25 @@ class ModelViaCep {
         if (this.validaCep() == true) {
             const request = new XMLHttpRequest()
             request.addEventListener('load',() => {
-                if(request.status == 200) {
+                if(request.responseText != "{\n  \"erro\": \"true\"\n}") {
                     const objCep = this.processaResponse(request.responseText);
                     this.filtraDados(objCep);
                     //retirar esse console log quando pronto
                     console.log(objCep)
-                    //request.status é 200 mesmo quando é inválido.
-                    //testar com cep 84500000
-                    //retorna erro no console.
-                    //como tratar?
-                    console.log(request.status)
-                    //criar comunicação com a controller para imprimir(?)
-                } else{
-                    console.log(request.status + "erro")
+                    console.log(request.error)
+                    console.log(request)
+                    const limpa = new ControllerCadastro();
+                    limpa.erroLimpa();
+                } else {
+                    const erro = new ControllerCadastro();
+                    erro.imprimeErro();
+                    //criar função para imprimir erro.
                     alert('CEP incorreto!')
                 }
             })
             request.open('GET', `https://viacep.com.br/ws/${cepUser}/json/`, false);
             request.send();
         } else {
-            //cep é inválido.
-            //retirar alert -> view
-            //alert("Formato de CEP inválido.");
             const erro = new ControllerCadastro();
             erro.imprimeErro();
         }
@@ -43,14 +40,6 @@ class ModelViaCep {
         return objCep;
     }
 
-    /*
-    imprime(objCep){
-        $('#inputEndereco').val(objCep.logradouro);
-        $('#inputBairro').val(objCep.bairro);
-        $('#inputCidade').val(objCep.localidade);
-        $('#inputEstado').val(objCep.uf);
-    }*/
-
     validaCep() {
         const cepUser = $('#inputCEP').val();
         if(cepUser != '') {
@@ -58,21 +47,8 @@ class ModelViaCep {
             if(validaCep.test(cepUser)) {
                 return true;
             }
-        } else {
-            //retirar alert -> view
-            //alert("Formato de CEP vazio.");
         }
-        
     }
-
-    /*
-    limpaForm() {
-        $('#inputCEP').val("");
-        $('#inputEndereco').val("");
-        $('#inputBairro').val("");
-        $('#inputCidade').val("");
-        $('#inputEstado').val("");
-    }*/
 
     filtraDados(request) {
         this.cep = request.cep;
