5630fd22391332e7f3f036084a33d92f2b3d22d6
danbaziewicz <danxbazi@gmail.com>
2022-05-22 21:20:27 -0300

    feat: adicionado requisição de limpeza de erro.

--- js/controllers/controller.js
+++ js/controllers/controller.js
@@ -2,18 +2,24 @@ class ControllerCadastro {
     adicionaCep() {
         const dadosModel = new ModelViaCep();
         dadosModel.requisicaoApi();
+
+        //TIRAR
         console.log(`entrei na controller ${dadosModel.getRua()}`)
 
-        //isolar em outro método
-        //pensar na comunicação com model.
         const view = new ViewFormulario();
         view.imprime(dadosModel);
-
     }
-    
+
+    erroLimpa() {
+        const view = new ViewFormulario();
+        view.limpaErro();
+    }
+        
     imprimeErro() {
+        if($('#inputCEP').val() !== ''){
         const erro = new ViewFormulario();
         erro.imprimeErro();
+        }
     }
 }
 
